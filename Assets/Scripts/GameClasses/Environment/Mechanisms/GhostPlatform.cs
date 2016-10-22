9ac85590f97f1409d6b97174066efe718af52233
TTTheMaTTT <projectttower@gmail.com>
2016-10-22 12:08:42 +0300

    some mechanisms for 5th level

---
+++ Assets/Scripts/GameClasses/Environment/Mechanisms/GhostPlatform.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Платформа, что может появляться, либо исчезать
+/// </summary>
+public class GhostPlatform : MonoBehaviour, IMechanism
+{
+
+    #region fields 
+
+    protected Animator anim;
+
+    #endregion fields
+
+    #region parametres
+
+    [SerializeField]
+    protected bool activated = true;
+
+    #endregion //parametres
+
+    public void Awake()
+    {
+        anim = GetComponent<Animator>();
+        if (anim)
+        {
+            anim.Play(activated ? "Appear" : "Disappear");
+        }
+    }
+
+    /// <summary>
+    /// Активировать механизм
+    /// </summary>
+    public void ActivateMechanism()
+    {
+        activated = !activated;
+        if (anim)
+        {
+            anim.Play(activated ? "Appear" : "Disappear");
+        }
+    }
+}
--- Assets/Scripts/GameClasses/Environment/Mechanisms/TwoStagesLeverScript.cs
+++ Assets/Scripts/GameClasses/Environment/Mechanisms/TwoStagesLeverScript.cs
@@ -4,7 +4,20 @@ using System.Collections;
 /// <summary>
 /// Скрипт, реализующий поведение рычага с двумя режимами - активный и неактивный рычаг
 /// </summary>
-public class TwoStagesLeverScript : MonoBehaviour
+public class TwoStagesLeverScript : LeverScript
 {
-
+    public override void Interact()
+    {
+        foreach (GameObject obj in mechanisms)
+        {
+            IMechanism mech = obj.GetComponent<IMechanism>();
+            if (mech != null)
+                mech.ActivateMechanism();
+        }
+        activated = !activated;
+        if (anim != null)
+        {
+            anim.Play(activated ? "Active" : "Inactive");
+        }
+    }
 }
